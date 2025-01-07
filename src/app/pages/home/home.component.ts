3627ecf766032f5d23b3e14781cf775b398055c7
sandipkurmi <sandipkurmi0@gmail.com>
2025-01-07 11:21:08 +0530

    refactor: simplify component structure and update styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: false,
-  
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
